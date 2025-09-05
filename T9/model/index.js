a8a42301eb31dfe2adc32b65cbf53ac1db4e6a71
vise07cs <vikram7cs@gmail.com>
2025-09-05 15:43:53 +0530

    One to many relationship implemented b/w student-department

--- T9/model/index.js
+++ T9/model/index.js
@@ -1,8 +1,14 @@
 const Student=require("./students");
 const Idcard=require("./idcard");
+const Department=require("./department");
 
-
+// one to one relationship
 Student.hasOne(Idcard);
 Idcard.belongsTo(Student);
 
-module.exports={Student,Idcard};
+
+// one to many relationship
+Department.hasMany(Student);
+Student.belongsTo(Department);
+
+module.exports={Student,Idcard,Department};
